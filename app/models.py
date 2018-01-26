1e3d04179036ebdbde2f8c733b3281fba7030d11
‟ElaineNerea” <ela.amish@Gmail.com>
2018-01-26 17:44:26 +0300

    create result function

--- app/models.py
+++ app/models.py
@@ -9,3 +9,14 @@ class Articles:
         self.urlToImage = urlToImage
         self.url = url
         self.publishedAt = publishedAt
+
+class Sources:
+    '''
+    Sources class to define sources objects
+    '''
+    def __init__(self,id,name,description,url,category):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.url = url
+        self.category = category
