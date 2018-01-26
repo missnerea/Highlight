846a4316fe6e64017c7c516eb826f47a5c7835bb
‟ElaineNerea” <ela.amish@Gmail.com>
2018-01-26 12:41:56 +0300

    Add files

---
+++ app/views.py
@@ -0,0 +1,17 @@
+from flask import render_template
+from app import app
+
+#Views
+@app.route('/')
+def index():
+    '''
+    View root page function that returns the index page and its data
+    '''
+    return render_template('index.html')
+
+@app.route('/article/<int:article_id>')
+def article(article_id):
+    '''
+    View article page function that returns the movie details page and its data
+    '''
+    return render_template('article.html',id = article_id)
